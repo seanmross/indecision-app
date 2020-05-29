968924fb02bab9a061713cbaf213aa1065b18a98
Sean Ross <seanross917@gmail.com>
2020-05-28 21:52:50 -0400

    set state with counter example

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ class Action extends React.Component {
 class Options extends React.Component {
     constructor(props) {
         super(props);
-        // this.handleRemoveAll = this.handleRemoveAll.bind(this);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
     }
 
     handleRemoveAll() {
@@ -54,7 +54,7 @@ class Options extends React.Component {
     render() {
         return (
           <div>
-            <button onClick={this.handleRemoveAll.bind(this)}>Remove All</button>
+            <button onClick={this.handleRemoveAll}>Remove All</button>
             {
                 this.props.options.map( (option, i) => <Option key={i} text={option} />)
             }
